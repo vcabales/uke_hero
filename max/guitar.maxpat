3c546ebb80da81ac0e79df97ae4e64ca73cced3b
vcabales <vcabales96@gmail.com>
2018-06-12 23:57:18 -0500

    reading from serial

--- max/guitar.maxpat
+++ max/guitar.maxpat
@@ -9,7 +9,7 @@
 			"modernui" : 1
 		}
 ,
-		"rect" : [ -50.0, 108.0, 933.0, 634.0 ],
+		"rect" : [ 115.0, 79.0, 933.0, 634.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var SerialPort = require('serialport');
 
-router.get('/', function(req,res,next) {
-	res.render('index.html');
+SerialPort.list(function (err,results) { //list all available ports
+  if (err) {
+    throw err;
+  }
+  console.log(results);
 });
+
+//constructing a SerialPort object immediately opens a port
+var port = new SerialPort('/dev/tty.usbmodem1411', {
+  baudRate: 9600
+  }, (err) => {
+  if (err) {
+    console.log('Error', err.message);
+  }
+});
+
+var portlist = SerialPort.list();
+console.log(portlist, function(err) {
+	if (err) {
+		console.log('Error: ', err.message);
+	}
+});
+
+port.on('data', function(data) {
+	console.log(data.toString());
+  // console.log(data);
+});
+
+// port.on('readable', function() {
+// 	console.log(port.read());
+// });
+
 
 module.exports = router;
