fe217fa289297a67d1a14e31f4990ee68839a57b
vcabales <vcabales96@gmail.com>
2018-06-13 00:44:28 -0500

    adding websockets

--- max/guitar.maxpat
+++ max/guitar.maxpat
@@ -9,7 +9,7 @@
 			"modernui" : 1
 		}
 ,
-		"rect" : [ 115.0, 79.0, 933.0, 634.0 ],
+		"rect" : [ 1314.0, 79.0, 2223.0, 1327.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 "use strict";
-
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 var SerialPort = require('serialport');
+var serialData = [];
+var ukeExists;
+
+router.get('/', function(req,res,next) {
+  res.render('index.html');
+});
 
 SerialPort.list(function (err,results) { //list all available ports
   if (err) {
@@ -18,19 +23,26 @@ var port = new SerialPort('/dev/tty.usbmodem1411', {
   }, (err) => {
   if (err) {
     console.log('Error', err.message);
+    ukeExists = false;
+  }
+  else {
+    ukeExists = true;
   }
 });
 
-var portlist = SerialPort.list();
-console.log(portlist, function(err) {
-	if (err) {
-		console.log('Error: ', err.message);
-	}
-});
-
 port.on('data', function(data) {
-	console.log(data.toString());
-  // console.log(data);
+	// console.log(data[0].toString());
+  var str = data.toString();
+  if (str.includes('255')) {
+    //start putting the data in serialData array
+  }
+  // serialData.push(data.toString());
+  // console.log(serialData);
+  // console.log(data.toString());
+}, function(err) {
+  if (err) {
+    console.log("Error: " + err.message);
+  }
 });
 
 // port.on('readable', function() {
